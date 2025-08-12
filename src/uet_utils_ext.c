de4db3909a93c87e96f5f5f345ea57c1f6e29018
Sanskar <sanskarjaiswal010@gmail.com>
2025-08-12 23:52:07 +0530

    ext utils

---
+++ src/uet_utils_ext.c
@@ -0,0 +1,7 @@
+#include "../include/uet_utils.h"
+#include <stdio.h>
+
+void debug_print_endpoint(SES_Endpoint_type *ep) {
+    printf("Endpoint JobID=%u, IPv4=%u, MTU=%u\n",
+           ep->JobID, ep->UET_Address.fa.addr.v4, ep->max_payload_size);
+}
